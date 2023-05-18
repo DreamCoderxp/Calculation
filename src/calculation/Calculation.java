e0b3924dbf238e77772fc438c942361bed548274
Gurleen <kau15124@sheridancollege.ca>
2023-05-18 15:14:39 -0400

    Calculation class is created.

---
+++ src/calculation/Calculation.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package calculation;
+
+/**
+ *
+ * @author Gurleen Kaur,2023
+
+ */
+public class Calculation {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x=5;
+        int y=10;
+        int result= x+y;
+        System.out.println(result);
+        
+        
+    }
+
+}
